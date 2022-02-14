56ca59ac817da9e6c05c98f51919c8f549aa0094
Allan Castellanos <allan_castellanos@discovery.com>
2022-02-14 14:59:02 -0600

    Add ESLint to workflow

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,5 @@
-import add from "./index";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const add = require("./index");
 
 test("adds 2 + 3 to equal 5", () => {
   expect(add(2, 3)).toBe(5);
